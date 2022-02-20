a6dd7bd457a0af058a0f0be443787d0d0dfda842
Gabriel <gabriel.kuka@miraclemill.com>
2022-02-20 19:51:30 +0100

    Added realtime price in dashboard

--- frontend/src/components/Trade/Dashboard/Header.js
+++ frontend/src/components/Trade/Dashboard/Header.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
+import { WebSocketContext } from "../../Test/websocket";
+import tradeService from "../../../services/trade";
+import Helper from "../../../services/Helper";
 
+import { useSelector } from "react-redux";
 import "./header.css";
 
 const Header = (props) => {
+  const stock = useSelector(({ socket }) => socket);
+  const ticker = useSelector(({ trade }) => trade.ticker);
+  const ws = useContext(WebSocketContext);
+
+  const [tickerData, setTickerData] = useState([]);
+  const prevClose = useRef();
+
+  const changeStyle = () => {
+    const changeColor = tickerData.changePercent > 0 ? "green" : "red";
+
+    return { color: changeColor, fontSize: "0.8rem" };
+  };
+
+  // Retrieve initial data
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await tradeService.getTickerPrice(ticker);
+      // Save change percent, price, and prev price
+      setTickerData(response);
+      prevClose.current = response.previousClose;
+    };
+
+    fetchData();
+
+    ws.subscribe(ticker);
+
+    return () => {
+      setTickerData([]);
+      ws.unsubscribe(ticker);
+    };
+  }, [ticker]);
+
+  // Check socket updates
+  useEffect(() => {
+    const change = Helper.formatNumber(stock.price - prevClose.current);
+    setTickerData({
+      price: stock.price,
+      change: change,
+      changePercent: Helper.formatChangePercent(change / prevClose.current),
+    });
+  }, [stock.price, ticker]);
+
   return (
     <div className="card-title">
-      <img id="logo" alt="Logo" src={props.header.logo} />
+      <img className={"logo"} alt="Logo" src={props.header.logo} />
       <span className="company-name">{props.header.name}</span>
+      <div className={"realtime-price-container"}>
+        <b>${tickerData.price}</b>
+        <span style={changeStyle()}>{tickerData.changePercent}%</span>
+      </div>
     </div>
   );
 };
