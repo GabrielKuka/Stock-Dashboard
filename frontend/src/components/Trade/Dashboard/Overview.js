c872298105a311b0e03845f1a4e53834046a4d87
Gabriel <gabriel.kuka@miraclemill.com>
2022-02-20 18:58:50 +0100

    Fixed another number format

--- frontend/src/components/Trade/Dashboard/Overview.js
+++ frontend/src/components/Trade/Dashboard/Overview.js
@@ -1,46 +1,61 @@
-import React from 'react'
+import React from "react";
+import Helper from "../../../services/Helper";
 
-const Overview = ({data})=>{
-    return (
-            <div className='container-fluid'>
-                <div className='row' >
-                    <div className='col-sm'>
-                        <label><b>CEO</b></label>
-                        <p>{data.CEO}</p>
-                    </div>
-                    <div className='col-sm'>
-                        <label><b>Industry</b></label>
-                        <p>{data.industry}</p>
-                    </div>
-                    <div className='col-sm'>
-                        <label><b>Sector</b></label>
-                        <p>{data.sector}</p>
-                    </div>
+const Overview = ({ data }) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm">
+          <label>
+            <b>CEO</b>
+          </label>
+          <p>{data.CEO}</p>
+        </div>
+        <div className="col-sm">
+          <label>
+            <b>Industry</b>
+          </label>
+          <p>{data.industry}</p>
+        </div>
+        <div className="col-sm">
+          <label>
+            <b>Sector</b>
+          </label>
+          <p>{data.sector}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm">
+          <label>
+            <b>Employees</b>
+          </label>
+          <p>{Helper.formatNumber(data.employees)}</p>
+        </div>
+        <div className="col-sm">
+          <label>
+            <b>Address</b>
+          </label>
+          <p>
+            {data.city} {data.state}, {data.country}
+          </p>
+        </div>
+        <div className="col-sm">
+          <label>
+            <b>Website</b>
+          </label>
+          <p>{data.website}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm">
+          <label>
+            <b>Description</b>
+          </label>
+          <p>{data.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-                </div>
-                <div className='row'>
-                    <div className='col-sm'>
-                        <label><b>Employees</b></label>
-                        <p>{data.employees}
-                        </p>
-                    </div>
-                    <div className='col-sm'>
-                        <label><b>Address</b></label>
-                        <p>{data.city} {data.state}, {data.country}</p>
-                    </div>
-                    <div className='col-sm'>
-                        <label><b>Website</b></label>
-                        <p>{data.website}</p>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-sm'>
-                        <label><b>Description</b></label>
-                        <p>{data.description}</p>
-                    </div>
-                </div>
-            </div> 
-    )
-}
-
-export default Overview
+export default Overview;
