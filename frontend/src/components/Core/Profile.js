d1624f279647babf7d7f1da3d2f5466559e466a7
Gabriel <gabriel.kuka@miraclemill.com>
2022-04-23 18:30:59 +0200

    Removed bootstrap

--- frontend/src/components/Core/Profile.js
+++ frontend/src/components/Core/Profile.js
@@ -4,22 +4,13 @@ import LoggedOut from "./LoggedOut";
 import StockListsPage from "../Trade/StockLists/StockListsPage";
 
 import "./style.css";
+import "./profile.scss";
 
 const ProfileItem = ({ data, label }) => {
-  const itemStyle = {
-    label: {
-      color: "gray",
-      textDecoration: "underline",
-      fontSize: "0.8rem",
-    },
-    data: {
-      fontSize: "1.3rem",
-    },
-  };
   return (
-    <div className="list-group-item">
-      <span style={itemStyle.label}>{label}</span>
-      <p style={itemStyle.data}>{data}</p>
+    <div className={"user-data-item"}>
+      <div className={"user-data-item__label"}>{label}</div>
+      <div className={"user-data-item__data"}>{data}</div>
     </div>
   );
 };
@@ -30,29 +21,17 @@ const Profile = ({ user }) => {
   }
 
   return (
-    <div className="row profile-wrapper">
-      <div className="col-md-4">
-        <div className="card profile-data shadow p-3 mb-5 ">
-          <div className="card-body">
-            <h2 className="card-title">{user.name}</h2>
-            <div className="list-group list-group-flush">
-              <ProfileItem data={user?.email} label="Email" />
-              <ProfileItem data={user?.phone} label="Phone Number" />
-              <ProfileItem data={user?.birthday} label="Date of Birth" />
-            </div>
-          </div>
-        </div>
+    <section className={"profile-wrapper"}>
+      <div className={"profile-wrapper__side"}>
+        <ProfileItem data={user?.name} label={"Username:"} />
+        <ProfileItem data={user?.email} label={"Email:"} />
+        <ProfileItem data={user?.birthday} label={"Birthdate:"} />
+        <ProfileItem data={user?.phone} label={"Phone number:"} />
       </div>
-      <div className="col-md-8">
-        <div className="card trades-watchlists shadow p-3 mb-5 rounded">
-          <div className="card-body">
-            <p className="card-title">Trades</p>
-            <div>Here we are going to list trades performed by the user</div>
-          </div>
-        </div>
+      <div className={"profile-wrapper__main"}>
         <StockListsPage user={user} />
       </div>
-    </div>
+    </section>
   );
 };
 
